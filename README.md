01be9dc746b76b842274bf9cc37db223a1db018b
Sylvan Mably <sylvan.m@ecobee.com>
2019-03-10 23:31:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## react-apollo bundling test
+
+`npm install && npm run build`
+
+Note the presence of `react-dom-server.browser.production.min.js` in the bundle map.
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [new BundleAnalyzerPlugin()]
+};
